825eb867b623f58f1e526a70ef0927b4fd57b5e2
Pedro Schuster <pedro.ssc42@gmail.com>
2023-01-07 14:05:03 -0300

    .gitignore added

--- PythonIntegration/Platforms/iOS/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace PythonIntegration;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
